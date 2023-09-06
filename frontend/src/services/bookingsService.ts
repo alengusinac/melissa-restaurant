d5c15f0d2ddacdf25a662c61ecd4ccfdae90c469
alengusinac <61206891+alengusinac@users.noreply.github.com>
2023-09-06 10:48:43 +0200

    added searchTablesByDate function

--- frontend/src/services/bookingsService.ts
+++ frontend/src/services/bookingsService.ts
@@ -1,9 +1,17 @@
 import { IBooking } from '../models/IBooking';
-import { get } from './serviceBase';
+import { ITable } from '../models/ITable';
+import { get, post } from './serviceBase';
 
 const BASE_URL = 'http://localhost:3000';
 
 export const getAllBookings = async (): Promise<IBooking[]> => {
   const response = await get<IBooking[]>(`${BASE_URL}/bookings/all`);
+  return response;
+};
+
+export const searchTablesByDate = async (date: string): Promise<ITable[]> => {
+  const response = await post<ITable[]>(`${BASE_URL}/bookings/search`, {
+    date: date,
+  });
   return response;
 };
