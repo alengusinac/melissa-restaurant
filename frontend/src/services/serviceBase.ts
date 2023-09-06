1059974fe2841fbd8cb5ed203c9221a7475b9ed4
alengusinac <61206891+alengusinac@users.noreply.github.com>
2023-09-06 10:48:18 +0200

    added post base service

--- frontend/src/services/serviceBase.ts
+++ frontend/src/services/serviceBase.ts
@@ -4,3 +4,8 @@ export const get = async <T>(url: string): Promise<T> => {
   const response = await axios.get<T>(url);
   return response.data;
 };
+
+export const post = async <T>(url: string, obj: object): Promise<T> => {
+  const response = await axios.post<T>(url, obj);
+  return response.data;
+};
