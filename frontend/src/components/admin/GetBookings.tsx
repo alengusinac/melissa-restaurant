f5fe33c518449854b4054bf386b61cdea85db141
etrigo1 <bjorn.sjostedt@gmail.com>
2023-09-07 10:52:01 +0200

    Boiler plate components and router adjustments

---
+++ frontend/src/components/admin/GetBookings.tsx
@@ -0,0 +1,5 @@
+const GetBookings = () => {
+  return <div>GetBookings</div>;
+};
+
+export default GetBookings;
---
+++ frontend/src/components/admin/HandleBookings.tsx
@@ -0,0 +1,13 @@
+import GetBookings from "./GetBookings";
+import Bookings from "./Bookings";
+
+const HandleBookings = () => {
+  return (
+    <section>
+      <GetBookings />
+      <Bookings />
+    </section>
+  );
+};
+
+export default HandleBookings;
