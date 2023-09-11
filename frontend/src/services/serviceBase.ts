172f523c661c16fcdb1831a68793cbb14690d1f8
alengusinac <61206891+alengusinac@users.noreply.github.com>
2023-09-11 11:47:33 +0200

    axios Acess buf fix

--- frontend/src/services/serviceBase.ts
+++ frontend/src/services/serviceBase.ts
@@ -2,12 +2,24 @@ import axios from 'axios';
 
 export const get = async <T>(url: string): Promise<T> => {
   axios.defaults.withCredentials = true;
-  const response = await axios.get<T>(url);
+  const response = await axios.get<T>(url, {
+    withCredentials: true,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'application/json',
+    },
+  });
   return response.data;
 };
 
 export const post = async <T>(url: string, obj: object): Promise<T> => {
   axios.defaults.withCredentials = true;
-  const response = await axios.post<T>(url, obj);
+  const response = await axios.post<T>(url, obj, {
+    withCredentials: true,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'application/json',
+    },
+  });
   return response.data;
 };
